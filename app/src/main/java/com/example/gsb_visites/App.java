668ff941df6bbc9d9c58c937426914994b4941cb
TISSOT <david.tissot@ac-grenoble.fr>
2026-04-20 17:25:43 +0200

    Configuration + Fragment Login

---
+++ app/src/main/java/com/example/gsb_visites/App.java
@@ -0,0 +1,9 @@
+package com.example.gsb_visites;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class App extends Application {}
+
---
+++ app/src/main/java/com/example/gsb_visites/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gsb_visites;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
