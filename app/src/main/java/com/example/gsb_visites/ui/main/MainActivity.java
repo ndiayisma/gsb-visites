3018ca214ef9180c97d3153c907970cf5f5e3a12
lgf-dti <david.tissot@ac-grenoble.fr>
2026-04-22 16:24:06 +0200

    Refactor MainActivity location + String resource update

--- app/src/main/java/com/example/gsb_visites/ui/main/MainActivity.java
+++ app/src/main/java/com/example/gsb_visites/ui/main/MainActivity.java
@@ -1,12 +1,10 @@
-package com.example.gsb_visites;
+package com.example.gsb_visites.ui.main;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.gsb_visites.R;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
